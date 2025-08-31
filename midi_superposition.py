0b58040a3df3d4f53b54d619b620dec7cd52ceeb
sinorywang <wangsinory@gmail.com>
2025-08-30 19:08:52 -0700

    Update midi_superposition.py

--- midi_superposition.py
+++ midi_superposition.py
@@ -1,39 +1,18 @@
 import pretty_midi
 import random
 
-def generate_versions(input_midi):
+def add_intervals_to_midi(input_midi, output_filename, interval_list):
     midi_data = pretty_midi.PrettyMIDI(input_midi)
-    #You can edit as you wish here
-    consonant_intervals = [3, 4, 7, 12]  
-    dissonant_intervals = [1, 6, 11]     
+    new_midi = pretty_midi.PrettyMIDI()
 
-    #Original
-    midi_data.write("Original.mid")
-
-    #Consonant
-    consonant_midi = pretty_midi.PrettyMIDI()
     for instrument in midi_data.instruments:
         new_instrument = pretty_midi.Instrument(program=instrument.program, is_drum=instrument.is_drum)
-        interval = random.choice(consonant_intervals)  
+        
         for note in instrument.notes:
             new_instrument.notes.append(note)
-            new_note = pretty_midi.Note(
-                velocity=note.velocity,
-                pitch=min(note.pitch + interval, 127), 
-                start=note.start,
-                end=note.end
-            )
-            new_instrument.notes.append(new_note)
-        consonant_midi.instruments.append(new_instrument)
-    consonant_midi.write("Consonant.mid")
 
-    # Dissonant
-    dissonant_midi = pretty_midi.PrettyMIDI()
-    for instrument in midi_data.instruments:
-        new_instrument = pretty_midi.Instrument(program=instrument.program, is_drum=instrument.is_drum)
-        interval = random.choice(dissonant_intervals) 
-        for note in instrument.notes:
-            new_instrument.notes.append(note)
+            interval = random.choice(interval_list)
+            
             new_note = pretty_midi.Note(
                 velocity=note.velocity,
                 pitch=min(note.pitch + interval, 127),
@@ -41,9 +20,22 @@ def generate_versions(input_midi):
                 end=note.end
             )
             new_instrument.notes.append(new_note)
-        dissonant_midi.instruments.append(new_instrument)
-    dissonant_midi.write("Dissonant.mid")
+            
+        new_midi.instruments.append(new_instrument)
+    
+    new_midi.write(output_filename)
 
-    print("Generated Original.mid / Consonant.mid / Dissonant.mid")
+def generate_versions(input_midi):
+    consonant_intervals = [3, 4, 7, 12]  
+    dissonant_intervals = [1, 6, 11]    
 
-generate_versions("input.mid")
+    original_midi = pretty_midi.PrettyMIDI(input_midi)
+    original_midi.write("Original.mid")
+
+    # Consonant
+    add_intervals_to_midi(input_midi, "Consonant.mid", consonant_intervals)
+
+    # Dissonant
+    add_intervals_to_midi(input_midi, "Dissonant.mid", dissonant_intervals)
+    
+    print("Generated: Original.mid / Consonant.mid / Dissonant.mid")
